59cfe3884c66d59f7104108a05ef79a7f222bca4
stenlee <lidan1811@gmail.com>
2018-08-28 16:58:31 +0200

    Initial Commit

---
+++ MincraftDicFazitFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MincraftDicFazitFinal
+//
+//  Created by Dan Li on 28.08.18.
+//  Copyright © 2018 DanStenLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
